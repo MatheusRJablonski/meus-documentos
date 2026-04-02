58aeafb5d99d7022ab563eb62362d44bda0d09e4
MatheusRJablonski <theus.jablonski@gmail.com>
2026-04-01 21:14:05 -0300

    a

--- eda/listaCSES.c
+++ eda/listaCSES.c
@@ -14,7 +14,6 @@ typedef struct {
     int x;
     int y;
     int dir[4];
-    
 } info;
 
 struct stack{
@@ -37,38 +36,34 @@ struct stack *cria(int tam){
     struct stack *stk = NULL;
     stk = malloc(sizeof(struct stack));
 
-    if(stk){
-        stk->vet = malloc(sizeof(info) * tam);
-        if(stk->vet){
-            stk->topo = -1;
-            stk->tam = tam;
-        }
-        else{
-            free(stk);
-            stk = NULL;
-        }
+    stk->vet = malloc(sizeof(info) * tam);
+    if(!stk->vet){
+        free(stk);
+        return NULL;
     }
+    stk->topo = -1;
+    stk->tam = tam;
     return stk;
 }
 struct stack *copia(struct stack *orig){ //gpt
+    if(!orig) return NULL;
     struct stack *nova = cria(orig->tam);
     nova->topo = orig->topo;
     memcpy(nova->vet, orig->vet, sizeof(info)*(orig->topo+1));
     return nova;
 }
 int empilha(info *reg, struct stack *stk){
-    
     if(!cheia(stk)){
-        (stk->topo)++;
-        memcpy(&(stk->vet[stk->topo]), reg, sizeof(info));
-        return 1;    
+        stk->topo++;
+        stk->vet[stk->topo] = *reg;
+        return 1;
     }
     return 0;
 }
 
 int desempilha(struct stack *stk){
     if(!vazia(stk)){
-        (stk->topo)--;
+        stk->topo--;
         return 1;
     }
     return 0;
@@ -83,7 +78,6 @@ int busca(info *reg,struct stack *stk){
 }
 
 struct stack *destroi(struct stack *stk){
-    
     if(stk){
         free(stk->vet);
         free(stk);
@@ -91,11 +85,12 @@ struct stack *destroi(struct stack *stk){
     return NULL;
 }
 int main(){
+    int n, m; 
+    scanf("%d %d", &n, &m);
 
-    int n, m; scanf("%d %d", &n, &m);
     struct stack *caminho = cria(n * m);
     struct stack *caminho2 = cria(n * m);
-    info inicio;
+    info inicio = {0};
     char labirinto[n][m], vis[n][m];
     for(int i = 0;i < n;i++){
         for(int j = 0;j < m;j++){
@@ -107,10 +102,11 @@ int main(){
             vis[i][j] = 0;
         }
     }
-    info posicaoDeSaida = {m, n};
+
     int menorCaminho = 1e9;
     int achou = 0;
     int i = inicio.x, j = inicio.y;
+    for(int d = 0; d < 4; d++) inicio.dir[d] = 0;
     empilha(&inicio, caminho);
     vis[i][j] = 1;
     int dx[4] = {0, 1, 0, -1}; // direita, baixo, esquerda, cima
@@ -119,42 +115,43 @@ int main(){
     while(!vazia(caminho)){
         int moveu = 0;
 
-        printf("Posicao atual (%d,%d) = %c, topo=%d\n", i, j, labirinto[i][j], caminho->topo);
         if(labirinto[i][j] == 'B'){
             achou = 1;
             if((caminho->topo + 1) < menorCaminho){
                 menorCaminho = caminho->topo + 1;
+                destroi(caminho2);
                 caminho2 = copia(caminho);
             }
         } else{
             for(int k = 0;k < 4;k++){
                 int ni = i + dx[k];
                 int nj = j + dy[k];
-                if(ni >= 0 && ni < n && nj >= 0 && nj < m && (labirinto[ni][nj] == '.' || labirinto[ni][nj] == 'B') && vis[ni][nj] == 0){
-                    
-                    j = nj;
-                    i = ni;
-                    info a = {i, j};
-                    if(caminho->vet[caminho->topo].dir[k] != 1){
+
+                if(ni >= 0 && ni < n && nj >= 0 && nj < m &&(labirinto[ni][nj] == '.' || labirinto[ni][nj] == 'B') &&vis[ni][nj] == 0){
+                    if(caminho->topo >= 0 && caminho->vet[caminho->topo].dir[k] != 1){
                         caminho->vet[caminho->topo].dir[k] = 1;
+                        i = ni;
+                        j = nj;
+                        info a = {0};
+                        a.x = i;        //mudou nd 
+                        a.y = j;
+                        for(int d = 0; d < 4; d++) a.dir[d] = 0;
                         empilha(&a, caminho);
                         vis[i][j] = 1;
                         moveu = 1;
-                    
                         break;
                     }
                 }
             }
         }
         if(!moveu){
+            vis[i][j] = 0; //mudou de linha
             desempilha(caminho);
             if(vazia(caminho)) break;
             info topo;
             busca(&topo, caminho);
-            vis[i][j] = 0;
             i = topo.x;
             j = topo.y;
-            
         }
     }
     if(!achou){
@@ -190,22 +187,9 @@ int main(){
         reverse_str(s, idx);
         printf("%s\n", s);
     }
+
+    destroi(caminho);
+    destroi(caminho2);
+
+    return 0;
 }
-
-
-/*
-5 8
-########
-#..#..##
-####..##
-#B....A#
-####..## 
-
-5 8
-########
-#..#..##
-########
-#B....A#
-####..## 
-
-*/
