cb2b31141ba81e81469b51cf39871a2131076ec0
MatheusRJablonski <theus.jablonski@gmail.com>
2026-09-04 19:36:20 -0300

    Fix vector size initialization in d.cpp

--- programaçãoCompetitiva/d.cpp
+++ programaçãoCompetitiva/d.cpp
@@ -7,7 +7,7 @@ int main(){
     int n,m;
     cin >> n >> m;
     int maior = max(n,m);
-    vector<vector<char>>a(n,vector<char>(maior));
+    vector<vector<char>>a(n,vector<char>(m));
     for(int i =0;i<n;i++){
         for(int j = 0;j<m;j++) cin >> a[i][j];
     }
